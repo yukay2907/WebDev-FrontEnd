9d76e11a94650bf9de2154aaab7cbc7d028d315d
Utkarsh Kashyap <116485473+yukay2907@users.noreply.github.com>
2025-08-24 15:34:50 +0530

    Made changes to index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const body = document.querySelector('body');
+const signInsSignUpButton = document.querySelector('.sign-in-sign-up');
 
 window.addEventListener('load', () => {
     body.classList.add("visible")
-})
+});
+
+signInsSignUpButton.addEventListener('click', (e) => {
+    location.href = './pages/signInsignUp/authenticate.html';
+});
